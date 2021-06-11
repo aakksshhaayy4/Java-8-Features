5db97b59b0716aa137a912b6de5c29e94a329fb2
Akshay <akhareakshay2@gmail.com>
2021-06-11 16:42:33 +0530

    Simply getting empname - empno

---
+++ Basic Example/TestSortingByEmpId.java
@@ -0,0 +1,25 @@
+class Employee
+{
+	int eno;
+	String ename;
+
+	Employee(int eno, String ename)
+	{
+		this.eno=eno;
+		this.ename=ename;
+	}
+
+	public String toString()
+	{
+		return ename+" : "+eno;
+	}
+}
+
+class  TestSortingByEmpId
+{
+	public static void main(String[] args) 
+	{
+		Employee e = new Employee(7855545,"Akshay");	
+		System.out.println(e.toString());
+	}
+}
