5d015fcc6949d0b9f85694300b66d03a98218a08
Akshay <akhareakshay2@gmail.com>
2021-06-11 17:11:50 +0530

    just getting arraylist's elements

---
+++ Basic Example/TestGettingEmpId_ArrayList.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Employee
+{
+	int eno;
+	String ename;
+
+	Employee(int eno, String ename)
+	{
+		this.eno=eno;
+		this.ename=ename;
+	}
+
+	public String toString()
+	{
+		return ename+" : "+eno;
+	}
+}
+
+class  TestGettingEmpId_ArrayList
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<Employee> al = new ArrayList<Employee>();
+		al.add(new Employee(475855, "Akshay"));
+		al.add(new Employee(548648, "Pavan"));
+		al.add(new Employee(154785, "Soham"));
+		al.add(new Employee(877558, "Shradha"));
+		al.add(new Employee(458798, "Pooja"));
+		al.add(new Employee(218794, "Shruti"));
+		al.add(new Employee(548797, "Harish"));
+
+		System.out.println(al);
+	}
+}
